2a7f1075285411e0c461b40a2925f371a745c8e3
Prashanth Rai <rai.prashanth@gmail.com>
2023-03-30 10:47:10 +0200

    first commit

---
+++ app.go
@@ -0,0 +1,146 @@
+/*
+Copyright 2022 IBM All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/hyperledger/fabric-gateway/pkg/client"
+	"github.com/joho/godotenv"
+)
+
+const (
+	channelName   = "mychannel"
+	chaincodeName = "events"
+)
+
+var now = time.Now()
+var assetID = fmt.Sprintf("asset%d", now.Unix()*1e3+int64(now.Nanosecond())/1e6)
+
+func main() {
+	clientConnection := newGrpcConnection()
+	defer clientConnection.Close()
+
+	id := newIdentity()
+	sign := newSign()
+
+	gateway, err := client.Connect(
+		id,
+		client.WithSign(sign),
+		client.WithClientConnection(clientConnection),
+		client.WithEvaluateTimeout(5*time.Second),
+		client.WithEndorseTimeout(15*time.Second),
+		client.WithSubmitTimeout(5*time.Second),
+		client.WithCommitStatusTimeout(1*time.Minute),
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer gateway.Close()
+
+	network := gateway.GetNetwork(channelName)
+
+	// Context used for event listening
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Listen for events emitted by subsequent transactions
+	startChaincodeEventListening(ctx, network)
+
+}
+
+func startChaincodeEventListening(ctx context.Context, network *client.Network) {
+	fmt.Println("\n*** Start chaincode event listening")
+
+	events, err := network.ChaincodeEvents(ctx, chaincodeName)
+	if err != nil {
+		panic(fmt.Errorf("failed to start chaincode event listening: %w", err))
+	}
+
+	go func() {
+		for event := range events {
+			executeFunctionCall(event.Payload)
+			if err != nil {
+				fmt.Printf("\n<-- Unable to unmarshal the venet payload : %s", formatJSON(event.Payload))
+			}
+			fmt.Printf("\n<-- Chaincode event received: %s - %s\n", event.EventName, formatJSON(event.Payload))
+		}
+	}()
+
+}
+func formatJSON(data []byte) string {
+	var result bytes.Buffer
+	if err := json.Indent(&result, data, "", "  "); err != nil {
+		panic(fmt.Errorf("failed to parse JSON: %w", err))
+	}
+	return result.String()
+}
+
+func handleEvent(data []byte, eventName string) {
+	//asset := formatJSON(data)
+	switch eventName {
+	case "CreateAsset":
+		executeFunctionCall(data)
+
+	case "UpdateAsset":
+		executeFunctionCall(data)
+	}
+
+}
+
+// Function to invoke HTTP Post to call the event handle azure function
+func executeFunctionCall(body []byte) error {
+	url := goDotEnvVariable("FUNCTION_HANDLER_HOST")
+	method := "POST"
+
+	// JSON body
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer res.Body.Close()
+
+	response, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Println(string(response))
+	return nil
+}
+
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+	// load .env file
+	err := godotenv.Load("connection.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// load .env file
+	envVariable := os.Getenv(key)
+
+	return envVariable
+}
