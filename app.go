859504da2ac9ba6f2fe36fe88f1b27a1330689ed
Prashanth Rai <rai.prashanth@gmail.com>
2023-04-02 08:44:40 +0200

    event handling for all events

--- app.go
+++ app.go
@@ -18,7 +18,6 @@ import (
 	"time"
 
 	"github.com/hyperledger/fabric-gateway/pkg/client"
-	"github.com/joho/godotenv"
 )
 
 const (
@@ -27,7 +26,7 @@ const (
 )
 
 var now = time.Now()
-var assetID = "101215487"
+var assetID = fmt.Sprintf("asset%d", now.Unix()*1e3+int64(now.Nanosecond())/1e6)
 
 func main() {
 	clientConnection := newGrpcConnection()
@@ -51,25 +50,127 @@ func main() {
 	defer gateway.Close()
 
 	network := gateway.GetNetwork(channelName)
-	contract := network.GetContract(chaincodeName)
+	// contract := network.GetContract(chaincodeName)
+
 	// Context used for event listening
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	// Listen for events emitted by subsequent transactions
 	startChaincodeEventListening(ctx, network)
-	updateAsset(contract)
-}
-func updateAsset(contract *client.Contract) {
-	fmt.Printf("\n--> Submit transaction: UpdateAsset, %s update appraised value to 200\n", assetID)
 
-	_, err := contract.SubmitTransaction("UpdateTicketStatus", assetID, "Accepted")
-	if err != nil {
-		panic(fmt.Errorf("failed to submit transaction: %w", err))
-	}
+	// firstBlockNumber := createAsset(contract)
+	// updateAsset(contract)
+	// transferAsset(contract)
+	// deleteAsset(contract)
 
-	fmt.Println("\n*** UpdateAsset committed successfully")
+	// // Replay events from the block containing the first transaction
+	// replayChaincodeEvents(ctx, network, firstBlockNumber)
 }
+
+// func createAsset(contract *client.Contract) uint64 {
+// 	fmt.Printf("\n--> Submit transaction: CreateAsset, %s owned by Sam with appraised value 100\n", assetID)
+
+// 	_, commit, err := contract.SubmitAsync("CreateAsset", client.WithArguments(assetID, assetID,
+// 		"ITXISP1412",
+// 		"AVATRICE ELECTROLUX TUTTO SPENTO",
+// 		"20230110",
+// 		"Z2",
+// 		"Amalia",
+// 		"Gennarelli",
+// 		"IT",
+// 		"Amalia.Gennarelli@gmail.com",
+// 		"069863536",
+// 		"Via Calipso 00042",
+// 		"Anzio",
+// 		"IT",
+// 		"00042",
+// 		"1239040",
+// 		"11",
+// 		"Nexure",
+// 		"20201007",
+// 		"20221007",
+// 		"914916405",
+// 		"20201007",
+// 		"Grando",
+// 		"2 LW",
+// 		"00",
+// 		"03700396"))
+// 	if err != nil {
+// 		panic(fmt.Errorf("failed to submit transaction: %w", err))
+// 	}
+
+// 	status, err := commit.Status()
+// 	if err != nil {
+// 		panic(fmt.Errorf("failed to get transaction commit status: %w", err))
+// 	}
+
+// 	if !status.Successful {
+// 		panic(fmt.Errorf("failed to commit transaction with status code %v", status.Code))
+// 	}
+
+// 	fmt.Println("\n*** CreateAsset committed successfully")
+
+// 	return status.BlockNumber
+// }
+
+// func updateAsset(contract *client.Contract) {
+// 	fmt.Printf("\n--> Submit transaction: UpdateAsset, %s update appraised value to 200\n", assetID)
+
+// 	_, err := contract.SubmitTransaction("UpdateTicketStatus", assetID, "blue")
+// 	if err != nil {
+// 		panic(fmt.Errorf("failed to submit transaction: %w", err))
+// 	}
+
+// 	fmt.Println("\n*** UpdateAsset committed successfully")
+// }
+
+// func transferAsset(contract *client.Contract) {
+// 	fmt.Printf("\n--> Submit transaction: TransferAsset, %s to Mary\n", assetID)
+
+// 	_, err := contract.SubmitTransaction("UpdateTicketStatus", assetID, "White")
+// 	if err != nil {
+// 		panic(fmt.Errorf("failed to submit transaction: %w", err))
+// 	}
+
+// 	fmt.Println("\n*** TransferAsset committed successfully")
+// }
+
+// func deleteAsset(contract *client.Contract) {
+// 	fmt.Printf("\n--> Submit transaction: DeleteAsset, %s\n", assetID)
+
+// 	_, err := contract.SubmitTransaction("DeleteAsset", assetID)
+// 	if err != nil {
+// 		panic(fmt.Errorf("failed to submit transaction: %w", err))
+// 	}
+
+// 	fmt.Println("\n*** DeleteAsset committed successfully")
+// }
+
+// func replayChaincodeEvents(ctx context.Context, network *client.Network, startBlock uint64) {
+// 	fmt.Println("\n*** Start chaincode event replay")
+
+// 	events, err := network.ChaincodeEvents(ctx, chaincodeName, client.WithStartBlock(startBlock))
+// 	if err != nil {
+// 		panic(fmt.Errorf("failed to start chaincode event listening: %w", err))
+// 	}
+
+// 	for {
+// 		select {
+// 		case <-time.After(10 * time.Second):
+// 			panic(errors.New("timeout waiting for event replay"))
+
+// 		case event := <-events:
+// 			asset := formatJSON(event.Payload)
+// 			fmt.Printf("\n<-- Chaincode event replayed: %s - %s\n", event.EventName, asset)
+
+// 			if event.EventName == "DeleteAsset" {
+// 				// Reached the last submitted transaction so return to stop listening for events
+// 				return
+// 			}
+// 		}
+// 	}
+// }
 
 func startChaincodeEventListening(ctx context.Context, network *client.Network) {
 	fmt.Println("\n*** Start chaincode event listening")
@@ -104,17 +205,22 @@ func handleEvent(data []byte, eventName string) {
 	//asset := formatJSON(data)
 	switch eventName {
 	case "CreateAsset":
-		executeFunctionCall(data)
+		executeFunctionCall(data, goDotEnvVariable("FUNCTION_HANDLER_UPDATE"))
+	case "UpdateTicket":
+		executeFunctionCall(data, goDotEnvVariable("FUNCTION_HANDLER_UPDATE"))
+	case "SubmitClaim":
+		executeFunctionCall(data, goDotEnvVariable("FUNCTION_HANDLER_CLAIM"))
+	case "UpdateTicketStatus":
+		executeFunctionCall(data, goDotEnvVariable("FUNCTION_HANDLER_UPDATE"))
+	case "AddParts":
+		executeFunctionCall(data, goDotEnvVariable("FUNCTION_HANDLER_UPDATE"))
 
-	case "UpdateAsset":
-		executeFunctionCall(data)
 	}
 
 }
 
 // Function to invoke HTTP Post to call the event handle azure function
-func executeFunctionCall(body []byte) error {
-	url := goDotEnvVariable("FUNCTION_HANDLER_HOST")
+func executeFunctionCall(body []byte, url string) error {
 	method := "POST"
 
 	// JSON body
